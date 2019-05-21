8eb2a5cda63442462045b1bb9698598c3fb0e1d2
siddharthp538 <svpandey86@gmail.com>
2019-05-21 15:46:46 +0530

    proof of work added

--- dev/blockchain.js
+++ dev/blockchain.js
@@ -38,4 +38,14 @@ Blockchain.prototype.hashBlock = function(prevBlockHash, currentBlockData, nonce
     return hash;
 }
 
+Blockchain.prototype.proofOfWork = function(prevBlockHash,  currentBlockData){
+    let nonce = 1;
+    let hash = this.hashBlock(prevBlockHash, currentBlockData, nonce);
+    while(hash.substring(0,4) !== '0000'){
+        nonce++;
+        hash = this.hashBlock(prevBlockHash,currentBlockData,nonce);
+    }
+    return nonce;
+
+}
 module.exports = Blockchain;
