5bbe0875f7bc0caf32329b44c1c07b8c80dce4fc
siddharthp538 <svpandey86@gmail.com>
2019-05-21 15:32:30 +0530

    sha256 added

--- dev/blockchain.js
+++ dev/blockchain.js
@@ -1,3 +1,4 @@
+const sha256 = require('sha256');
 function Blockchain(){
     this.chain = [];
     this.newTransactions = [];
@@ -12,7 +13,7 @@ Blockchain.prototype.createNewBlock = function(nonce, prevHashBlock, hash){
         prevHashBlock: prevHashBlock,
         transactions: this.newTransactions
     }
-    this.transactions=[];
+    this.newTransactions=[];
     this.chain.push(newBlock);
     return newBlock;
 }
@@ -28,7 +29,13 @@ Blockchain.prototype.createNewTransaction = function(amount,sender,recipient) {
         recipient
     }
     this.newTransactions.push(newTransaction);
-    return newTransaction;
+    return this.getLastBlock()['index']+1;
+}
+
+Blockchain.prototype.hashBlock = function(prevBlockHash, currentBlockData, nonce ){
+    const data = prevHashBlock + nonce.toString() + JSON.stringify(currentBlockData);
+    const hash = sha256(data);
+    return hash;
 }
 
 module.exports = Blockchain;
--- dev/test.js
+++ dev/test.js
@@ -1,4 +1,7 @@
 const blockchain = require('./blockchain')
 const bitcoin = new blockchain();
-bitcoin.createNewBlock(2389,'sdgdgdfs','afgdfgfbgv');
+bitcoin.createNewBlock(234,'ASDGFG4HG567','5678IASGFDHGF');
+bitcoin.createNewTransaction(80,'Siddharth','Pandey');
+bitcoin.createNewBlock(127,'YUIO09876','74186DFGHJKL');
+
 console.log(bitcoin)
